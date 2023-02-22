80d26d6ed4e881232040725669363c960c7ef137
Daniel Korpon <dk@stellargrove.com>
2023-02-21 23:33:14 -0500

    updates to push distributions out

--- distributions.py
+++ distributions.py
@@ -1,7 +1,25 @@
-import pandas as pd
+import numpy as np
+import scipy.stats as stats
 
-def generateDistributionData(distribution: string = "normal", numberOfPoints):
+def createPoissonData(mu, n):
+    return stats.poisson.rvs(mu=mu,size=n)
 
-    data = pd.DataFrame()
+def createUniformData(begin,width,n):
+    return stats.uniform.rvs(loc=begin,width=width,size=n)
+
+def createNormalData(mean,std,size):
+    return stats.norm.rvs(mean,std,size)
+
+def createGammaData(alpha, size):
+    return stats.gamma.rvs(alpha,size)
+
+# This requires that you pass the scale as equal to 1/lambda
+def createExponentialData(scale,location,size):
+    return stats.expon.rvs(scale=(scale),loc=location,size=size)
+
+def createBinomialData(n, p, size):
+    return stats.binom.rvs(n,p,size)
+
+def createBernoulliData(p,size):
+    return stats.bernoulli.rvs(p = p,size = size)
     
-    return data
