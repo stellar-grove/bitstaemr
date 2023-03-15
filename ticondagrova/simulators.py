aa947105f80b2eedfe17286a751be98fa5849237
Daniel Korpon <dk@stellargrove.com>
2023-03-15 13:24:19 -0400

    updates to bring files over to public repo

---
+++ ticondagrova/simulators.py
@@ -0,0 +1,283 @@
+dk_repo = "C:/repo/bitstaemr"
+sg_repo = "C:/stellar-grove/bitstaemr"
+import sys;sys.path.append(dk_repo);sys.path.append(sg_repo)
+import pandas as pd
+from utils import tools
+import distributions as dists
+from distributions import DaCountDeMonteCarlo as dcmc
+import numpy as np
+from num2words import num2words
+
+def generatePersonalityType():
+    # 0 Miserable, 1 Debbie Downer, 2 Even Steven, 3 Optimistic Oliver, 4 Richard Fucking Simons
+    # Generate a random number between 0 and 4 to designate the personality type.
+    personalityType = int(np.floor(dcmc.createUniformData(dcmc,0, 5, 1)[0]))
+    return personalityType
+
+class PERT(object):
+    def __init__(self,config={}) -> None:
+        self.config = config
+        self.stats = {"error_details": []}
+        self.data = {}
+    
+    def get_working_directories(self):
+        wd = tools.get_llaves("workingDirectory")
+        return wd
+
+    def set_distribution(self, distribution_number = 1):
+        dict_distribution = {
+            1:"TSP",
+            2:"PERT",
+            3:"Herrerias"
+        }
+        self.config["distribution"] = dict_distribution[distribution_number]
+    
+    def generate_data(self):
+        df = pd.DataFrame()
+        return df
+
+class biostats(object):
+    def __init__(self,config={}) -> None:
+        self.config = config
+        self.stats = {"error_details": []}
+        self.data = {}
+        self.patient = {}
+         
+    def clearPatient(self):
+        self.patient = {}
+
+    def createPatient(self):
+        self.generateGender()
+        self.generateAge()
+        self.generateBP()
+        self.generateCholesterol()
+        self.generateBMI()
+        self.generatePhysicalActivity()
+        self.generateDiabetes()
+        self.generateSWLS()
+        return self.patient
+
+    def generateGender(self):
+        genderValue = np.round(np.random.rand(),0)
+        self.patient["genderValue"] = genderValue
+        if genderValue == 1: 
+            self.patient["genderName"] = "Female"
+        else: self.patient["genderName"] = "Male"
+    
+    def generateAge(self):
+        sample = np.random.rand()
+        mean = 60
+        std = 20
+        age = dcmc.sampleFromNormal(dcmc,mean = mean, std = std, sample = sample)
+        age = np.round(age,0)
+        self.patient["age"] = age
+        
+
+    def generateBP(self):
+        gender = self.patient["genderValue"]
+        if gender == 1: bp = dcmc.sampleFromNormal(self,mean=120,std=10,sample=np.random.rand())
+        else: bp = dcmc.sampleFromNormal(self,mean=110,std=20,sample=np.random.rand())
+        self.patient["systolicBP"] = bp
+        if gender == 1: bp = dcmc.sampleFromNormal(self,mean=120,std=10,sample=np.random.rand())
+        else: bp = dcmc.sampleFromNormal(self,mean=75,std=20,sample=np.random.rand())
+        self.patient["diastolicBP"] = bp
+
+    def generateCholesterol(self):
+        gender = self.patient["genderValue"]
+        age = self.patient["age"]
+        # Case Men Over 40
+        if gender == 1 and age > 40:
+            sample = np.random.rand()
+            mean = 40; std = 10
+            LDL = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 60; std = 20
+            HDL = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 50; std = 10
+            TRYG = dcmc.sampleFromNormal(dcmc, mean, std, sample)
+        # Case Men Under 40
+        if gender == 1 and age < 40:
+            sample = np.random.rand()
+            mean = 30; std = 10
+            LDL = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 70; std = 20
+            HDL = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 35; std = 10
+            TRYG = dcmc.sampleFromNormal(dcmc, mean, std, sample)
+        # Case Women Over 40
+        if gender == 0 and age > 40:
+            sample = np.random.rand()
+            mean = 20; std = 5
+            LDL = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 80; std = 20
+            HDL = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 60; std = 10
+            TRYG = dcmc.sampleFromNormal(dcmc, mean, std, sample)
+        # Case Women Under 40
+        if gender == 0 and age < 40:
+            sample = np.random.rand()
+            mean = 20; std = 5
+            LDL = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 70; std = 20
+            HDL = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 30; std = 10
+            TRYG = dcmc.sampleFromNormal(dcmc, mean, std, sample)
+
+        self.patient["HDL"] = HDL
+        self.patient["LDL"] = LDL
+        self.patient["TRY"] = TRYG
+
+    def generateBMI(self):
+        gender = self.patient["genderValue"]
+        age = self.patient["age"]
+        sample = np.random.rand()
+        # Case Men Over 40
+        if gender == 1 and age > 40:
+            mean = 40; std = 10
+            BMI = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+        # Case Men Under 40
+        if gender == 1 and age < 40:
+            mean = 30; std = 10
+            BMI = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+        # Case Women Over 40
+        if gender == 0 and age > 40:
+            mean = 20; std = 5
+            BMI = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+        # Case Women Under 40
+        if gender == 0 and age < 40:
+            mean = 20; std = 5
+            BMI = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+        
+        self.patient["BMI"] = BMI
+    
+    def generatePhysicalActivity(self):
+        activityLevel = np.floor(np.random.rand() * 7)
+        self.patient["PhysicalActivity"] = activityLevel
+    
+    def generateSmokingStatus(self):
+        smokingStatus = np.floor(np.random.rand())
+        self.patient["PhysicalActivity"] = smokingStatus
+    
+    def generateFamilyHistory(self):
+        familyHistory = np.floor(np.random.rand())
+        self.patient["FamilyHistory"] = familyHistory
+    
+    def generateDiabetes(self):
+        gender = self.patient["genderValue"]
+        age = self.patient["age"]
+        # Case Men Over 40
+        if gender == 1 and age > 40:
+            sample = np.random.rand()
+            mean = 40; std = 10
+            HBA1C = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 60; std = 20
+            ORAL = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 50; std = 10
+            FPG = dcmc.sampleFromNormal(dcmc, mean, std, sample)
+        # Case Men Under 40
+        if gender == 1 and age < 40:
+            sample = np.random.rand()
+            mean = 30; std = 10
+            HBA1C = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 70; std = 20
+            ORAL = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 35; std = 10
+            FPG = dcmc.sampleFromNormal(dcmc, mean, std, sample)
+        # Case Women Over 40
+        if gender == 0 and age > 40:
+            sample = np.random.rand()
+            mean = 20; std = 5
+            HBA1C = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 80; std = 20
+            ORAL = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 60; std = 10
+            FPG = dcmc.sampleFromNormal(dcmc, mean, std, sample)
+        # Case Women Under 40
+        if gender == 0 and age < 40:
+            sample = np.random.rand()
+            mean = 20; std = 5
+            HBA1C = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 70; std = 20
+            ORAL = dcmc.sampleFromNormal(dcmc,mean, std, sample)
+            mean = 30; std = 10
+            FPG = dcmc.sampleFromNormal(dcmc, mean, std, sample)
+        
+        self.patient["HBA1C"] = HBA1C
+        self.patient["ORAL"] = ORAL
+        self.patient["FPG"] = FPG
+
+    def generateMedicalHistory(self):
+        medicationUse = np.floor(np.random.rand())
+        self.patient["MedicationUse"] = medicationUse
+
+    def generateSWLS(self):
+        personalityType = generatePersonalityType()
+        # Create the bounds of how high the personality can score on each of the 5 questions
+        listBounds = [
+                        [0,3], # Miserable
+                        [1,4], # Debbie Downer
+                        [2,5], # Even Steven
+                        [3,6], # Optimistic Oliver
+                        [4,7]  # Richard Fucking Simons
+                      ]
+        responseBounds = listBounds[personalityType]
+        low = responseBounds[0]
+        high = responseBounds[1]
+        responseList = dcmc.createUniformData(dcmc,a = low, b = high, sampleSize = 5)
+        responseList = [int(response) for response in responseList]
+        dictResponse = {}
+        for index, response in enumerate(responseList):
+            questionNumber = num2words(index + 1)
+            dictResponse[f"{questionNumber}"] = response
+        self.patient["SWLS"] = dictResponse
+
+    def generatePSS(self):
+        personalityType = generatePersonalityType()
+        # Create the bounds of how high the personality can score on each of the 5 questions
+        listBounds = [
+                        [0,1], # Miserable
+                        [1,2], # Debbie Downer
+                        [2,3], # Even Steven
+                        [3,4], # Optimistic Oliver
+                        [4,4]  # Richard Fucking Simons
+                      ]
+        responseBounds = listBounds[personalityType]
+        low = responseBounds[0]
+        high = responseBounds[1]
+        responseList = dcmc.createUniformData(dcmc,a = low, b = high, sampleSize = 10)
+        responseList = [int(response) for response in responseList]
+        dictResponse = {}
+        for index, response in enumerate(responseList):
+            questionNumber = num2words(index + 1)
+            dictResponse[f"{questionNumber}"] = response
+        self.patient["PSS"] = dictResponse
+    
+    def generateSTAI(self):
+        personalityType = generatePersonalityType()
+        # Create the bounds of how high the personality can score on each of the 5 questions
+        listBounds = [
+                        [0,1], # Miserable
+                        [1,2], # Debbie Downer
+                        [2,3], # Even Steven
+                        [3,4], # Optimistic Oliver
+                        [4,4]  # Richard Fucking Simons
+                      ]
+        responseBounds = listBounds[personalityType]
+        low = responseBounds[0]
+        high = responseBounds[1]
+        responseList = dcmc.createUniformData(dcmc,a = low, b = high, sampleSize = 10)
+        responseList = [int(response) for response in responseList]
+        dictResponse = {}
+        for index, response in enumerate(responseList):
+            questionNumber = num2words(index + 1)
+            dictResponse[f"{questionNumber}"] = response
+        self.patient["PSS"] = dictResponse
+
+    def generatePatientDescription(self):
+        description = f"""
+        The patient is a {self.patient["age"]} year-old {self.patient["genderName"]}.
+        Their vitals are:
+            BP: {self.patient["systolicBP"]} / {self.patient["diastolicBP"]}.
+            Cholesterol: HDL: {self.patient["HDL"]}, LDL: {self.patient["LDL"]}, Triglycerides: {self.patient["TRY"]}.
+
+        """
+        self.patient["PatientDescription"] = description.strip()
