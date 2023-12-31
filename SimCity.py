e79a6157154957374a43df3fc1d85cc1eb353d74
korpon <108818506+korpon@users.noreply.github.com>
2023-12-31 16:16:11 -0500

    first push, broke out mod simy py work into it's own module named SimCity (#8)

---
+++ SimCity.py
@@ -0,0 +1,26 @@
+import modsim as sim
+
+class bikeshare(object):
+
+
+    def __init__(self,config={}) -> None:
+        self.config = config
+        self.stats = {"error_details": []}
+        self.data = {}
+        
+  
+    def setStates(self, lstStates):
+        a = lstStates[0]
+        b = lstStates[1]
+        self.data["state"] = sim.State(spotA = a, spotB = b)
+
+    def ItemToA(self, sharemodel):
+        sharemodel.spotA +=1
+        sharemodel.spotB -=1
+        self.data["state"] = sharemodel
+    
+    def ItemToB(self, sharemodel):
+        sharemodel.spotA -=1
+        sharemodel.spotB +=1
+        self.data["state"] = sharemodel
+        
