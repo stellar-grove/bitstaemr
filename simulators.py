7dcd200741e9c1629e60319f5e12d5c939981363
Funk Doctor Spock <dk@stellargrove.com>
2024-01-25 19:00:03 -0500

    adding monkies which has some crypto stuffs and other fun things.

--- simulators.py
+++ simulators.py
@@ -381,6 +381,40 @@ class MSP(object):
         sm.spotA -= 1
         sm.spotB += 1
 
+
+    def make_system(beta, gamma):
+        init = sim.State(s = 89, i = 1, r = 0)
+        init /= init.sum()
+        return sim.System(init=init, t_end=7*14,beta=beta, gamma=gamma)
+
+    def update_func(t, state, system):
+        s, i, r = state.s, state.i, state.r
+
+        infected = system.beta * i * s
+        recovered = system.gamma * i
+
+        s -= infected
+        i += infected - recovered
+        r += recovered
+
+        return sim.State(s=s, i=i, r=r)
+
+
+    def plot_results(S, I, R):
+        S.plot(style='--', label='Susceptible')
+        I.plot(style='-', label='Infected')
+        R.plot(style=':', label='Resistant')
+        sim.decorate(xlabel='Time (days)',
+                ylabel='Fraction of population')
+       
+    def run_simulation(system, update_func):
+        frame = sim.TimeFrame(columns=system.init.index)
+        frame.loc[0] = system.init
+        for t in range(0, system.t_end):
+            frame.loc[t+1] = update_func(t, frame.loc[t], system)
+        
+        return frame
+
 #|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 #|                       Planetary Orbits                                                     |
 #|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
@@ -443,7 +477,7 @@ class GrokSays(object):
         self.config = config
         self.stats = {"error_details": []}
         self.data = {}
-
+"""
     # Load the genome sequence
     def load_genome(file_path):
         genome_seq = SeqIO.read(file_path, 'fasta')
@@ -496,3 +530,4 @@ class GrokSays(object):
     # classified_creature = classify_creature(training_data, test_data)
     # print(f'The creature is classified as: {classified_creature}')
 
+"""
