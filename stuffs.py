938a65eda65c8584a0fd3950602f5fcfc36f92e2
Funk Doctor Spock <dk@stellargrove.com>
2024-02-26 05:42:02 -0500

    adding AlphaVantage and a notebook. more stuff to stuffs.

---
+++ stuffs.py
@@ -0,0 +1,33 @@
+import os
+
+
+# ------ Begin Folders ------ #
+DATA_FOLDER = "C:/stellar-grove/tara/data/"
+DATA_FOLDER_SONGROVE = "C:/stellar-grove/tara/SongroveBotanicals/data/"
+
+
+# ------ Begin Constants ----- #
+peep = os.environ["USERNAME"]
+robot = os.environ["COMPUTERNAME"]
+homeD = f'{os.environ["HOMEDRIVE"]}{os.environ["HOMEPATH"]}'.replace('\\','/')
+
+SGWD = f'{homeD}/Stellar Grove/'
+bitsWD = f'{SGWD}bitstaemr - Documents/'
+taraWD = f'{SGWD}ticondagrova - Documents/'
+
+kaggleWD = f'{taraWD}Kaggle/'
+server = f'{robot}\SQLEXPRESS'
+sniffnet = 'sniffnet.database.windows.net'
+DB_tara = {'servername': server,
+        'database': 'tara',
+        'driver': 'driver=SQL Server Native Client 11.0'
+        ,'tgtSchema':'pll'
+        ,'tgtTbl':'player_stats'
+        ,'fileRoot':'C:/stellar-grove/tara/data/pll/'}
+
+dbAzureTARA = {'servername': sniffnet,
+            'database': 'tara',
+            'driver': 'driver=SQL Server Native Client 11.0'
+                }
+
+
